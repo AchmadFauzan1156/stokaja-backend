8dbd8c6990bf41791700259cf8b437602273aa07
AchmadFauzan1156 <achmadfauzan_1156@students.unnes.ac.id>
2026-05-27 19:26:36 +0700

    fix: add force IPv4

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -3,11 +3,14 @@ const nodemailer = require('nodemailer');
 const sendEmail = async (options) => {
     // 1. Buat transporter
     const transporter = nodemailer.createTransport({
-        service: process.env.EMAIL_SERVICE || 'gmail',
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true, 
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS
-        }
+        },
+        family: 4
     });
 
     // 2. Definisikan opsi email
